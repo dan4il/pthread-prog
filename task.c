6a97a3179f04b40ec5d4f8c6c4784e62a046a45c
dan4il <1danbers1@gmail.com>
2021-04-28 14:41:20 +0300

    added 3rd thread

--- task.c
+++ task.c
@@ -3,13 +3,17 @@
 #include <unistd.h>
 #include <stdlib.h>
 #define SIZE 100
-#define MAX 10000000
+#define MAX 100000
 
-int start=0;//start*2 -first value of array
-int glob[SIZE+1]={};
-// glob[SIZE]=0 -not ready for output =1-ready for output
+int even_start=0;//start*2 -first value of even array
+int odd_start=0;//start*2+1 - first values of odd array
+unsigned int glob[2*SIZE+1]={};
+// glob[2*SIZE]=0 -not ready for output;=1 one part(even part)-ready for output;
+  // =2-ready for output
+
 pthread_mutex_t lock=PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t cond_write,cond_calc;
+pthread_cond_t cond_write,cond_calc_even,cond_calc_odd;
+
 void* f_write(void* arg)
 {
   FILE* file;
@@ -17,20 +21,26 @@ void* f_write(void* arg)
   if(file!=NULL)
   {
     printf("work with file started..\n");
-    for(int k=0;k<MAX;k+=SIZE)
+    for(int k=0;k<MAX;k++)
     {
       pthread_mutex_lock(&lock);
-      while(glob[SIZE]!=1)       pthread_cond_wait(&cond_write,&lock);
+      while(glob[2*SIZE]!=2)       pthread_cond_wait(&cond_write,&lock);
 
       for(int i=0;i<SIZE;i++)
       {
           fprintf(file,"%d\t",glob[i]);
 
       }
-      glob[SIZE]=0;
+      fprintf(file, "\n" );//to customize output
+      for(int i=SIZE;i<2*SIZE;i++)
+      {
+          fprintf(file,"%d\t",glob[i]);
+
+      }
+      glob[2*SIZE]=0;
       fprintf(file, "\n" );
-      printf("put in file 100 values\n");
-      pthread_cond_signal(&cond_calc);
+      printf("put in file 200 values\n");
+      pthread_cond_signal(&cond_calc_even);
       pthread_mutex_unlock(&lock);
       }
       printf("-----------\n" );
@@ -47,48 +57,69 @@ void* f_write(void* arg)
 
 void* calc_even(void* arg)
 {
-  for(int k=0;k<MAX;k+=SIZE)
+  for(int k=0;k<MAX;k++)
   {
     pthread_mutex_lock(&lock);
-    while(glob[SIZE]!=0)     pthread_cond_wait(&cond_calc,&lock);
+    while(glob[2*SIZE]!=0)     pthread_cond_wait(&cond_calc_even,&lock);
 
     for(int i=0;i<SIZE;i++)
     {
-      glob[i]=start*2;
-      start++;
+      glob[i]=even_start*2;
+      even_start++;
 
     }
-    glob[SIZE]=1;
-    printf("array full of values\n");
-    pthread_cond_signal(&cond_write);
+    glob[2*SIZE]=1;
+    printf("array full of even values\n");
+    pthread_cond_signal(&cond_calc_odd);
     pthread_mutex_unlock(&lock);
 
   }
   pthread_exit(0);
 }
 
-// void* calc_odd(void* arg)
-// {
-//   for(int i=0;i<SIZE;i++)
-//     glob[i]=1+i*2;
-//   return NULL;
-// }
+void* calc_odd(void* arg)
+{
+
+  for(int k=0;k<MAX;k++)
+  {
+    pthread_mutex_lock(&lock);
+    while(glob[2*SIZE]!=1)     pthread_cond_wait(&cond_calc_odd,&lock);
+
+    for(int i=SIZE;i<2*SIZE;i++)
+      {
+        glob[i]=1+odd_start*2;
+        odd_start++;
+      }
+      glob[2*SIZE]=2;
+      printf("array full of odd values\n");
+      pthread_cond_signal(&cond_write);
+      pthread_mutex_unlock(&lock);
+
+  }
+
+
+  pthread_exit(0);
+}
 int main()
 {
 
-  pthread_t thread1,thread2;
+  pthread_t thread1,thread2,thread3;
   pthread_cond_init(&cond_write,NULL);
-  pthread_cond_init(&cond_calc,NULL);
+  pthread_cond_init(&cond_calc_even,NULL);
+  pthread_cond_init(&cond_calc_odd,NULL);
 
   pthread_create(&thread1,NULL,calc_even,NULL);
+  pthread_create(&thread3,NULL,calc_odd,NULL);
   pthread_create(&thread2,NULL,f_write,NULL);
   pthread_join(thread1,NULL);
   pthread_join(thread2,NULL);
-
+  pthread_join(thread3,NULL);
 
   pthread_mutex_destroy(&lock);
   pthread_cond_destroy(&cond_write);
-  pthread_cond_destroy(&cond_calc);
+  pthread_cond_destroy(&cond_calc_even);
+  pthread_cond_destroy(&cond_calc_odd);
+
   return 0;
 
 }
